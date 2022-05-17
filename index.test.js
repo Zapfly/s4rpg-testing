19967e2422d0967640a75df2c52241db278557d3
Zapfly <seegerlucas295@gmail.com>
2022-05-16 20:21:28 -0600

    feat: can use config object to create Creatures

--- index.test.js
+++ index.test.js
@@ -8,8 +8,25 @@ test("connection works", () => {
 
 test("Creature Creater", () => {
     let testCreature = new s4rpg.Creature(1, 1, 1, 1);
+    expect(testCreature.name).toEqual(undefined)
     expect(testCreature.strengh).toEqual(1)
     expect(testCreature.speed).toEqual(1)
     expect(testCreature.stealth).toEqual(1)
     expect(testCreature.surveilance).toEqual(1)
 });
+
+test("Creature Creation with config obj", () => {
+    let creatureConfig = {
+        name: "testCreature",
+        strengh: 1,
+        speed: 1,
+        stealth: 1,
+        surveilance: 1,
+    }
+    let testCreature = new s4rpg.Creature(creatureConfig);
+    expect(testCreature.strengh).toEqual(1)
+    expect(testCreature.speed).toEqual(1)
+    expect(testCreature.stealth).toEqual(1)
+    expect(testCreature.surveilance).toEqual(1)
+    expect(testCreature.name).toEqual("testCreature")
+})
