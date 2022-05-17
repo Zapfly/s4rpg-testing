4aa2fe8b284c0fe886ea5e438ab696ffa1085771
Zapfly <seegerlucas295@gmail.com>
2022-05-16 19:37:33 -0600

    test for creature creater

--- index.test.js
+++ index.test.js
@@ -4,5 +4,12 @@ import s4rpg from 's4rpg'
 
 test("connection works", () => {
     expect(s4rpg.testWord()).toEqual("hello World")
-})
+});
 
+test("Creature Creater", () => {
+    let testCreature = new s4rpg.Creature(1, 1, 1, 1);
+    expect(testCreature.strengh).toEqual(1)
+    expect(testCreature.speed).toEqual(1)
+    expect(testCreature.stealth).toEqual(1)
+    expect(testCreature.surveilance).toEqual(1)
+});
