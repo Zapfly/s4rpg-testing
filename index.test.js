05c4c7deb31a9e1f3becdacbd29a5000714fc2ba
Zapfly <seegerlucas295@gmail.com>
2022-05-16 21:33:44 -0600

    test with config obj

--- index.test.js
+++ index.test.js
@@ -9,7 +9,7 @@ test("connection works", () => {
 test("Creature Creater", () => {
     let testCreature = new s4rpg.Creature(1, 1, 1, 1);
     expect(testCreature.name).toEqual(undefined)
-    expect(testCreature.strengh).toEqual(1)
+    expect(testCreature.strength).toEqual(1)
     expect(testCreature.speed).toEqual(1)
     expect(testCreature.stealth).toEqual(1)
     expect(testCreature.surveilance).toEqual(1)
@@ -18,15 +18,17 @@ test("Creature Creater", () => {
 test("Creature Creation with config obj", () => {
     let creatureConfig = {
         name: "testCreature",
-        strengh: 1,
-        speed: 1,
-        stealth: 1,
-        surveilance: 1,
+        strengh: 2,
+        speed: 3,
+        stealth: 7,
+        surveilance: 11,
     }
     let testCreature = new s4rpg.Creature(creatureConfig);
-    expect(testCreature.strengh).toEqual(1)
-    expect(testCreature.speed).toEqual(1)
-    expect(testCreature.stealth).toEqual(1)
-    expect(testCreature.surveilance).toEqual(1)
-    expect(testCreature.name).toEqual("testCreature")
+
+    expect(testCreature.strengh).toEqual(2);
+    expect(testCreature.speed).toEqual(3);
+    expect(testCreature.stealth).toEqual(7);
+    expect(testCreature.surveilance).toEqual(11);
+    expect(testCreature.name).toEqual("testCreature");
+    // expect(testCreature.calcDamage()).toEqual(1000);
 })
